8b779f1c52dbbbe5a4d0f9867a375724d58216b2
Ronny230797 <ronny230797@gmail.com>
2019-05-31 21:01:40 -0600

    Union a BD

--- View/Presentacion/AddClient.cs
+++ View/Presentacion/AddClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,19 @@ namespace View
 {
     public partial class AddClient : Form
     {
+        //Cadena de conexion a la base de datos
+         String connectionString = @"server=localhost; port=3306; user id=root; password=Tortuguero.2011.; database=compuelecta;";
+        
+         MySqlConnection connection;
+
+        int idParty = 0;
+
+
         public AddClient()
         {
             InitializeComponent();
+            //Inizializar conexion
+            connection = new MySqlConnection(connectionString);
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -60,6 +71,26 @@ namespace View
 
         private void BtnSave_Click(object sender, EventArgs e)
         {
+            //Save Data Client
+
+            //Abrir conexion
+            connection.Open();
+            //Seleccionar Stored y conexion
+            MySqlCommand mySqlCom = new MySqlCommand("AddOrEditClient",connection);
+            //Definir que se va a usar el stored
+            mySqlCom.CommandType = CommandType.StoredProcedure;
+            //Parametros recibe el stored procedure
+            mySqlCom.Parameters.AddWithValue("_IdParty",idParty);
+            mySqlCom.Parameters.AddWithValue("_Name", txtName.Text.Trim());
+            mySqlCom.Parameters.AddWithValue("_LastName1", txtLastName1.Text.Trim());
+            mySqlCom.Parameters.AddWithValue("_LastName2", txtLastName2.Text.Trim());
+            mySqlCom.Parameters.AddWithValue("_Telephone1", Int32.Parse(txtPhone1.Text.Trim()));
+            mySqlCom.Parameters.AddWithValue("_Telephone2", Int32.Parse(txtPhone2.Text.Trim()));
+            mySqlCom.Parameters.AddWithValue("_Email", txtMail.Text.Trim());
+            mySqlCom.ExecuteNonQuery();
+            //Mensaje de completado
+            MessageBox.Show("Cliente Guardado");
+            this.Close();
 
         }
 
